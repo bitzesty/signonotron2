58d64dc73027f72834da30a9149962db3fb89c0b
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-02-19 21:20:41 +0300

    [SIDEKIQ] corrected settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,32 +1,8 @@
-# require "sidekiq"
-
-# if ENV['VCAP_SERVICES'].present?
-#   services = JSON.parse(ENV['VCAP_SERVICES'])
-#   if services['redis'].present?
-#     credentials = services['redis'].first['credentials']
-#     ENV['REDIS_URL'] = "redis://:#{credentials['password']}@#{credentials['host']}:#{credentials['port']}/"
-#   end
-# end
-
-# redis_config = { namespace: "signon_sidekiq" }
-# redis_config[:url] = ENV['REDIS_URL'] if ENV['REDIS_URL']
-
-# Sidekiq.configure_server do |config|
-#   config.redis = redis_config
-#   config.server_middleware do |chain|
-#     chain.add Sidekiq::Statsd::ServerMiddleware, env: 'govuk.app.signon', prefix: 'workers'
-#   end
-# end
-
-# Sidekiq.configure_client do |config|
-#   config.redis = redis_config
-# end
-
 require "sidekiq"
 
 # PaaS Redis is not ready for prod
 if ENV['VCAP_SERVICES'].present?
-  redis_url = JSON.parse(ENV["VCAP_SERVICES"])["user-provided"].select{ |s| s["name"] == "redis-alpha" }[0]["credentials"]["uri"]
+  redis_url = JSON.parse(ENV["VCAP_SERVICES"])["redis"].select{ |s| s["name"] == ENV["REDIS_INSTANCE_NAME"] }[0]["credentials"]["uri"]
 else
   redis_url = ENV["REDIS_URL"]
 end
